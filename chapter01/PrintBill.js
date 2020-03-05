ff2a088f96922963b6873ef1857208310ef28c16
SzilvasiPeter <peti.szilvasi95@gmail.com>
2020-03-05 21:19:26 +0100

    Extract switch statement

--- chapter01/PrintBill.js
+++ chapter01/PrintBill.js
@@ -6,25 +6,7 @@ function statement(invoice, plays){
 
     for(let perf of invoice.performances){
         const play = plays[perf.playID];
-        let thisAmount = 0;
-
-        switch (play.type){
-            case "tragedy":
-                thisAmount = 40000;
-                if(perf.audience > 30){
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if(perf.audience > 20){
-                    thisAmount += 1000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`Unknow type: ${play.type}`);
-        }
+        let thisAmount = amountFor(perf, paly);
 
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -40,5 +22,29 @@ function statement(invoice, plays){
 
         console.log(result);
         return result;
+    }
+
+    function amountFor(perf, play){
+        let thisAmount = 0;
+
+        switch (play.type) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 1000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience;
+                break;
+            default:
+                throw new Error(`Unknow type: ${play.type}`);
+        }
+
+        return thisAmount;
     }
 }
